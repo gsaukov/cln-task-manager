45d3aa14d4b3426e897ea9295a55826dcfb57d14
georgy.saukov <gsaukov@yahoo.com>
2021-11-26 18:57:09 +0100

    logging

--- src/main/java/com/celonis/challenge/controllers/projectgenerationtask/TaskController.java
+++ src/main/java/com/celonis/challenge/controllers/projectgenerationtask/TaskController.java
@@ -2,6 +2,8 @@ package com.celonis.challenge.controllers.projectgenerationtask;
 
 import com.celonis.challenge.model.projectgenerationtask.ProjectGenerationTask;
 import com.celonis.challenge.services.projectgeneration.TaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final TaskService taskService;
 
@@ -27,7 +31,9 @@ public class TaskController {
 
     @PostMapping("/")
     public ProjectGenerationTask createTask(@RequestBody @Valid ProjectGenerationTask pgTask) {
-        return taskService.createTask(pgTask);
+        taskService.createTask(pgTask);
+        logger.info("Created task id: " + pgTask.getId());
+        return pgTask;
     }
 
     @GetMapping("/{taskId}")
@@ -38,19 +44,23 @@ public class TaskController {
     @PutMapping(path = "/{taskId}")
     public ProjectGenerationTask updateTask(@PathVariable String taskId,
                                             @RequestBody @Valid ProjectGenerationTask updatePgTask) {
-        return taskService.update(taskId, updatePgTask);
+        ProjectGenerationTask existingPgTask = taskService.update(taskId, updatePgTask);
+        logger.info("Updated task id: " + existingPgTask.getId());
+        return existingPgTask;
     }
 
     @DeleteMapping("/{taskId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteTask(@PathVariable String taskId) {
         taskService.delete(taskId);
+        logger.info("Deleted task id: " + taskId);
     }
 
     @PostMapping("/{taskId}/execute")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void executeTask(@PathVariable String taskId) {
         taskService.executeTask(taskId);
+        logger.info("Executed task id: " + taskId);
     }
 
     @GetMapping("/{taskId}/result")
