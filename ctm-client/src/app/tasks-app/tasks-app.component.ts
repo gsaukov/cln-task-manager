8e65a70386d09efaad0b72abd75eae5d265eaae7
georgy.saukov <gsaukov@yahoo.com>
2021-12-03 21:42:00 +0100

    task-5 tasks app

---
+++ ctm-client/src/app/tasks-app/tasks-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-app',
+  templateUrl: './tasks-app.component.html',
+  styleUrls: ['./tasks-app.component.css']
+})
+export class TasksAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
