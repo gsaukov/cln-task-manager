fb389cfc8512de18ee710e29f5b08e6a9500478d
georgy.saukov <gsaukov@yahoo.com>
2021-12-05 14:26:27 +0100

    task-5 counter task paginator

--- ctm-client/src/app/tasks-app/counter-task-app/counter-task-pagination/counter-task-pagination.component.ts
+++ ctm-client/src/app/tasks-app/counter-task-app/counter-task-pagination/counter-task-pagination.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {CounterTask} from "../service/rest/model/counterTask";
 
 @Component({
   selector: 'app-counter-task-pagination',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter-task-pagination.component.scss']
 })
 export class CounterTaskPaginationComponent implements OnInit {
+  @Input() data: CounterTask[]
 
   constructor() { }
 
