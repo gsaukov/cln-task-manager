7db9fd9198722ca648e4d6a7037d7744d691380e
georgy.saukov <gsaukov@yahoo.com>
2021-12-05 13:41:15 +0100

    task-5 header interceptor

---
+++ ctm-client/src/app/header-interceptor.ts
@@ -0,0 +1,14 @@
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export class HeaderInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const clonedRequest = req.clone({ headers: req.headers.append('Celonis-Auth', 'totally_secret') });
+    return next.handle(clonedRequest);
+  }
+}
