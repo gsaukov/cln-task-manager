23eddb9c9627e9fe30a78cb9f668624ecf1dfcb4
georgy.saukov <gsaukov@yahoo.com>
2021-12-06 00:09:07 +0100

    task-5 preoject generation task readme notes.

--- ctm-client/src/app/tasks-app/project-generation-task-app/project-generation-task-app.component.ts
+++ ctm-client/src/app/tasks-app/project-generation-task-app/project-generation-task-app.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-project-generation-task-app',
   templateUrl: './project-generation-task-app.component.html',
-  styleUrls: ['./project-generation-task-app.component.css']
+  styleUrls: ['./project-generation-task-app.component.scss']
 })
 export class ProjectGenerationTaskAppComponent implements OnInit {
 
